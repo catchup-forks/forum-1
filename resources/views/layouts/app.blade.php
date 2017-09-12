ffdecc01a355aee3b4683997742d5ce46ac4ae60
Jimmie Johansson <jimmitjoo@gmail.com>
2017-09-12 22:47:58 +0200

    fix description and titles for social media

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,11 +11,31 @@
 
     <link rel="shortcut icon" href="/images/favicon.png">
 
-    @if (isset($title))
+    @if (isset($page) && isset($page->seo_title))
+        <title>{{ $title }}</title>
+        <meta property="og:title" content="{{ $title }}"/>
+        <meta name="twitter:title" content="{{ $title }}"/>
+
+    @elseif (isset($title))
         <title>{{ $title . ' - ' . config('app.name', 'Laravel') }}</title>
+        <meta property="og:title" content="{{ $title . ' - ' . config('app.name', 'Laravel') }}"/>
+        <meta name="twitter:title" content="{{ $title . ' - ' . config('app.name', 'Laravel') }}"/>
+
     @else
         <title>{{ config('app.name', 'Laravel') }}</title>
+        <meta property="og:title" content="{{ config('app.name', 'Laravel') }}"/>
+        <meta name="twitter:title" content="{{ config('app.name', 'Laravel') }}"/>
+
     @endif
+
+    @if (isset($page) && isset($page->seo_description))
+        <meta name="description" content="{{ $page->seo_description }}">
+        <meta property="og:description" content="{{ $page->seo_description }}"/>
+        <meta name="twitter:description" content="{{ $page->seo_description }}"/>
+    @endif
+
+    <meta property="og:site_name" content="{{ config('app.name', 'Laravel') }}"/>
+    <meta property="og:image" content="https://mate.run/images/MateRun_logo.png"/>
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
@@ -32,59 +52,77 @@
         .flex {
             flex: 1;
         }
+
         .level {
             display: flex;
             align-items: center;
         }
+
         .mt-1 {
             margin-top: 1em;
         }
+
         .mr-1 {
             margin-right: 1em;
         }
+
         .mb-1 {
             margin-bottom: 1em;
         }
+
         .ml-1 {
             margin-left: 1em;
         }
+
         .mob-100 {
             display: inline-block;
         }
-        @media(max-width: 767px) {
+
+        @media (max-width: 767px) {
             .mob-100 {
                 width: 100%;
             }
+
             .xs-mt-1 {
                 margin-top: 1em;
             }
+
             .xs-mr-1 {
                 margin-right: 1em;
             }
+
             .xs-mb-1 {
                 margin-bottom: 1em;
             }
+
             .xs-ml-1 {
                 margin-left: 1em;
             }
         }
+
         .btn-facebook {
             background-color: #3B5998;
             color: #fff;
         }
+
         .btn-facebook:hover {
             background-color: #3B4584;
             color: #fff;
         }
+
         .btn-google {
             background-color: #d34836;
             color: #fff;
         }
+
         .btn-google:hover {
             background-color: #BF4836;
             color: #fff;
         }
-        [v-cloak] { display: none }
+
+        [v-cloak] {
+            display: none
+        }
     </style>
 </head>
 <body>
